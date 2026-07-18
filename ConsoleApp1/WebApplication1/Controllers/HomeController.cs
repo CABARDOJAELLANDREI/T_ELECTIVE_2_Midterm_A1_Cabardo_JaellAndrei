cf01025109d223adb713ea14a8a425b56bc24174
STUDENT <STUDENT@DESKTOP-HN6O76J>
2026-07-18 12:05:24 +0800

    update

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -1,24 +1,74 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
 
-namespace WebApplication1.Controllers;
-
-public class HomeController : Controller
+namespace WebApplication1.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home";
+            ViewData["Welcome"] = "Welcome to my Portfolio!";
+            ViewData["Name"] = "Hello, I'm Jaell Andrei";
+            ViewData["Description"] =
+                "Hi! My Name is Jaell Andrei O. Cabardo, 3rd year BSIT student at Lyceum of Alabang. I'm interested in Database, Web Development, Cybersecurity, and Cobot Integration.";
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About";
+
+            ViewData["Biography"] =
+                "I am Jaell Andrei Cabardo, a 3rd-year BSIT student actively building a diverse foundation in software development, databases, cybersecurity, and web technologies.";
+
+            ViewData["Goal"] =
+                "My goal is to become a software developer capable of building secure, responsive, and user-friendly web applications.";
+
+            ViewData["Education"] = "Bachelor of Science in Information Technology - Currently Taking";
+
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            ViewData["Title"] = "Skills";
+
+            ViewData["Languages"] = "C#, Java, Python, SQL, HTML, CSS, JavaScript";
+            ViewData["Framework"] = "ASP.NET Core MVC, Bootstrap 5, REST API";
+            ViewData["Database"] = "MS SQL Server, Entity Framework Core";
+            ViewData["Developer Tools"] = "Visual Studio, Git, GitHub, Pycharm, Dart Studio, XG5000, XPBuilder";
+
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            ViewData["Title"] = "Projects";
+
+            ViewData["Project1"] = "Parking Management System";
+            ViewData["Project2"] = "Loan Calculator";
+            ViewData["Project3"] = "Parking Management System";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Title"] = "Contact";
+
+            ViewData["Email"] = "jaellandreic@gmail.com";
+            ViewData["Phone"] = "+63 (993) 335-1307";
+            ViewData["Github"] = "https://github.com/CABARDOJAELLANDREI";
+            ViewData["LinkedIn"] = "https://www.linkedin.com/";
+            ViewData["Facebook"] = "https://facebook.com/";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
